b6d8abbc73d10fc61f60feee0b3f164bbebdc9c3
isuru <isuru@wadinternational.net>
2020-05-10 18:26:34 +0530

    Project init

---
+++ Assesment-Planner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assesment-Planner
+//
+//  Created by Isuru Wijesinghe on 5/10/20.
+//  Copyright © 2020 Isuru Wijesinghe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
